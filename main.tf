a1acd7f6ac9a214d8bf04c35bf7f770ce4b8285a
Vivek Raj <vr32288@gmail.com>
2024-06-14 09:08:46 +0530

    added 8 resources and file and remote_exec provisioners

--- main.tf
+++ main.tf
@@ -1,1 +1,109 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+resource "aws_key_pair" "example" {
+  key_name = "terraform-demo-vivek"
+  public_key = file("~/.ssh/id_rsa.pub")
+}
+
+
+resource "aws_vpc" "vpc1" {
+  cidr_block = var.cidr
+}
+
+resource "aws_subnet" "subnet1" {
+  vpc_id = aws_vpc.vpc1.id
+  cidr_block = "10.0.0.0/24"
+  availability_zone = "us-east-1a"
+  map_public_ip_on_launch = true
+}
+
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.vpc1.id
+}
+
+resource "aws_route_table" "rt" {
+  vpc_id = aws_vpc.vpc1.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+}
+
+resource "aws_route_table_association" "rta1" {
+  subnet_id = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.rt.id
+}
+
+resource "aws_security_group" "sg1" {
+  name = "web"
+  vpc_id = aws_vpc.vpc1.id
+
+  ingress {
+    description = "HTTP from VPC"
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "SSH"
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "Web-ig"
+  }
+}
+
+
+resource "aws_instance" "server" {
+  ami = "ami-00beae93a2d981137"
+  instance_type = "t2.micro"
+  key_name = aws_key_pair.example.key_name
+  vpc_security_group_ids = [aws_security_group.sg1.id]
+  subnet_id = aws_subnet.subnet1.id
+
+
+  connection {
+    type = "ssh"
+    user = "ubuntu"
+    private_key = file("~/.ssh/id_rsa")
+    host = self.public_ip
+  }
+
+  provisioner "file" {
+    source = "app.py"
+    destination = "/home/ubuntu/app.py"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'Hello from the remote instance'",
+      "sudo apt update -y",  # Update package lists (for ubuntu)
+      "sudo apt-get install -y python3-pip",  # Example package installation
+      "cd /home/ubuntu",
+      "sudo pip3 install flask",
+      "sudo python3 app.py &",
+    ]
+  }
+}
 
